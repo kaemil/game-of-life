8d3bc6ec8cb5b3647dedaf27a91492957c982952
kaemil <67312346+kaemil@users.noreply.github.com>
2021-10-28 17:30:59 +0200

    Adding working game

--- src/App.js
+++ src/App.js
@@ -1,8 +1,137 @@
-import React from 'react';
-import './App.css';
+import React, { useCallback, useRef, useState } from 'react';
+import produce from 'immer';
+import './css/index.css';
+const colsNumber = 50;
+const rowsNumber = 50;
+
+const neighboursPosition = [
+	[-1, -1],
+	[0, -1],
+	[1, -1],
+	[-1, 0],
+	[1, 0],
+	[-1, 1],
+	[0, 1],
+	[1, 1],
+];
+
+const createEmptyBoard = () => {
+	const rows = [];
+	for (let i = 0; i < rowsNumber; i++) {
+		rows.push(Array(colsNumber).fill(0));
+	}
+	return rows;
+};
+
+const createRandomBoard = () => {
+	const rows = [];
+	for (let i = 0; i < rowsNumber; i++) {
+		rows.push(
+			Array.from(Array(colsNumber), () => (Math.random() > 0.5 ? 1 : 0)),
+		);
+	}
+	return rows;
+};
 
 function App() {
-	return <div>xd</div>;
+	const [working, setWorking] = useState(false);
+	const [board, setBoard] = useState(() => {
+		return createEmptyBoard();
+	});
+	const handleClick = (i, j) => {
+		const newBoard = produce(board, (boardCopy) => {
+			boardCopy[i][j] = board[i][j] ? 0 : 1;
+		});
+		setBoard(newBoard);
+	};
+
+	const workingRef = useRef(working);
+	workingRef.current = working;
+
+	const handleStart = () => {
+		setWorking(!working);
+		if (!working) {
+			workingRef.current = true;
+			startSimulation();
+		}
+	};
+	const handleClear = () => {
+		setBoard(() => {
+			return createEmptyBoard();
+		});
+	};
+	const handleRandom = () => {
+		setBoard(() => {
+			return createRandomBoard();
+		});
+	};
+	const startSimulation = useCallback(() => {
+		if (!workingRef.current) {
+			console.log('koniec');
+			return;
+		}
+		setBoard((b) => {
+			return produce(b, (boardCopy) => {
+				for (let i = 0; i < rowsNumber; i++) {
+					for (let j = 0; j < colsNumber; j++) {
+						let neighbours = 0;
+						neighboursPosition.forEach(([x, y]) => {
+							const newI = i + x;
+							const newJ = j + y;
+							if (
+								newI >= 0 &&
+								newI < rowsNumber &&
+								newJ >= 0 &&
+								newJ < colsNumber
+							) {
+								neighbours += b[newI][newJ];
+							}
+						});
+
+						if (neighbours < 2 || neighbours > 3) {
+							boardCopy[i][j] = 0;
+						} else if (b[i][j] === 0 && neighbours === 3) {
+							boardCopy[i][j] = 1;
+						}
+					}
+				}
+			});
+		});
+		setTimeout(startSimulation, 200);
+	}, []);
+
+	return (
+		<div>
+			<button onClick={handleStart}>{working ? 'stop' : 'start'}</button>
+			<button onClick={handleClear}>clear</button>
+			<button onClick={handleRandom}>random</button>
+			<div
+				style={{
+					display: 'grid',
+					gridTemplateColumns: `repeat(${colsNumber},20px)`,
+					backgroundColor: '#2C2C2C',
+				}}
+			>
+				{board.map((rows, i) =>
+					rows.map((col, j) => {
+						return (
+							<div
+								onClick={() => handleClick(i, j)}
+								key={`${i}-${j}`}
+								style={{
+									width: 20,
+									height: 20,
+									boxSizing: 'border-box',
+									border: '1px solid #212121',
+									backgroundColor: board[i][j] ? '#e65e8b' : null,
+								}}
+							></div>
+						);
+					}),
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
